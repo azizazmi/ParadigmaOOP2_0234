9c9e91c79f46b217ce2772962a333e1a4fb3b769
Azizah A. Azmi <azizah.aurellia.ft23@mail.umy.ac.id>
2024-05-28 14:53:57 +0700

    membuat class turunan pekerja

--- solveMultiwaris.cpp
+++ solveMultiwaris.cpp
@@ -9,3 +9,11 @@ public:
         cout << "Orang dibuat dengan umur " << umur << "\n" << endl;
     }
 };
+
+class pekerja : public orang {
+public:
+
+    pekerja(int pUmur) : orang(pUmur) {
+        cout << "Pekerja dibuat\n" << endl;
+    }
+};
